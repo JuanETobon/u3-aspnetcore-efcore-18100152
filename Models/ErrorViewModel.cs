c09cb5bb0b9aa6ae1956a05098d2d72090208281
Juan Baltierrez <l18100152@nlaredo.tecnm.mx>
2021-12-03 01:03:24 -0600

    Creacion del repositorio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace u3_aspnetcore_efcore_18100152.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
